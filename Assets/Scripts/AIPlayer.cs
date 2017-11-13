922958f5956ddbc13f2ab60fc8b54b6f44cc88b2
Thomas Lee <thomasslee97@gmail.com>
2017-11-13 11:35:06 +0000

    Moved Scripts to Assets

---
+++ Assets/Scripts/AIPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPRTest1
+{
+    class AIPlayer : Player
+    {
+        public AIPlayer(int college) : base(college)
+        {
+
+        }
+
+        public void populate()
+        {
+
+        }
+    }
+}
