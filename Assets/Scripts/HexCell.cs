aa4b293f117cd04050ed431c6e45e845e9d0b354
Unknown <clw579@york.ac.uk>
2017-11-15 11:24:42 +0000

    HexMap push

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+	public HexCoordinates coordinates;
+
+	public Color color;
+
+	[SerializeField]
+	HexCell[] neighbors;
+}
