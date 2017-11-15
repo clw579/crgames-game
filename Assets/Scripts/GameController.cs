5526a5ed254833f498fda15cfeaefc041906f1b5
Thomas Lee <thomasslee97@gmail.com>
2017-11-15 10:17:40 +0000

    Fixed all imports

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SEPRTest1
+{
+    enum colleges
+    {
+        Unknown,
+        Alcuin,
+        Goodricke,
+        Langwith,
+        Constantine,
+        Halifax,
+        Vanbrugh,
+        Derwent,
+        James,
+        Wentworth,
+    }
+
+    class GameController : MonoBehaviour
+    {
+        
+        static void Main(string[] args)
+        {
+			GenerateMap ();
+        }
+
+		static void GenerateMap(){
+			Map map = new Map(128);
+			Console.Out.Write("test");
+		}
+
+		static void NextTurn(){
+
+		}
+    }
+}
