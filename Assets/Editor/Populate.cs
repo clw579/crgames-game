e32703ade7a0b24d9ba8691bf342fb1cb46d9719
ScottBaker98 <sb1875@york.ac.uk>
2017-12-16 14:05:00 +0000

    Testing - First batch skeletons

---
+++ Assets/Editor/Populate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Populate {
+
+	[Test]
+	public void PopulateSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator PopulateWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
