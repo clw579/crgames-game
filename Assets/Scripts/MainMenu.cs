d59c2e62267625a58d9aeee0fc7ac4bc2b07b1f4
Thomas Lee <thomasslee97@gmail.com>
2017-12-19 18:01:23 +0000

    Main Menu Commenting
    *Shouldn't you be studying?*

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
 
+/*
+    CLASS: MainMenu
+    FUNCTION: Handles functions for the main menu
+ */
+
 public class MainMenu : MonoBehaviour
 {
-
+    /// <summary>
+    /// Moves to the game scene.
+    /// </summary>
     public void PlayGame()
     {
+        // Load the next scene in the scene manager
         EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex + 1);
-
     }
 
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
     public void QuitGame()
     {
         Debug.Log("Quit");
