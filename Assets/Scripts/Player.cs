8a1ee6058ac0c3d4b6e7f555059f2bbc6357dabc
Thomas Lee <thomasslee97@gmail.com>
2017-12-20 15:25:36 +0000

    Player Commenting
    *10/10 would play again*

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,37 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+    CLASS: Player
+    FUNCTION: Keeps track of player properties such as college, name and owned tiles
+ */
 
 namespace CRGames_game
 {
     public class Player
     {
+        // The player's college
         private int college;
+        // The player's name
         private String name;
+        // The tiles that the player owns
         private List<Tile> ownedTiles;
+        // The number of gang members that this player owns
         private int noOfGangMembers;
        
-
+        /// <summary>
+        /// Initialises the player with only a college.
+        /// </summary>
+        /// <param name="college">The player's college.</param>
 		public Player(int college){
 
 		}
 
+        /// <summary>
+        /// Initialises the player with both a college and a name.
+        /// </summary>
+        /// <param name="college">The player's college.</param>
+        /// <param name="name">The player's name.</param>
         public Player(int college, String name)
         {
             this.college = college;
@@ -29,48 +45,65 @@ namespace CRGames_game
             noOfGangMembers = 0;
         }
 
-       
-
+        /// <summary>
+        /// Alerts the player that it's their turn, used by AIPlayer to carry out turn logic.
+        /// </summary>
         public void AlertItsMyTurn(){
 
 		}
 
+        /// <summary>
+        /// Gets the player's college.
+        /// </summary>
+        /// <returns>The player's college.</returns>
 		public int GetCollege (){
 			return college;
 		}
 
+        /// <summary>
+        /// Gets the player's name.
+        /// </summary>
+        /// <returns>The player's name.</returns>
         public string GetName()
         {
             return name;
         }
 
+        /// <summary>
+        /// Get the tiles owned by the player.
+        /// </summary>
+        /// <returns>The tiles owned by the player.</returns>
         public List<Tile> GetOwnedTiles()
         {
             return ownedTiles;
         }
 
+        /// <summary>
+        /// Adds a tile to the list of the player's owned tiles.
+        /// </summary>
+        /// <param name="tile">The tile to add.</param>
         public void AddOwnedTiles(Tile tile)
         {
             ownedTiles.Add(tile);
         }
 
+        /// <summary>
+        /// Gets the number of gang members owned by the player.
+        /// </summary>
+        /// <returns>The number of gang members owned by the player.</returns>
         public int GetNumberOfGangMembers()
         {
             return noOfGangMembers;
         }
         
+        /// <summary>
+        /// Sets the number of gang members owned by the player to the number of tiles they own and returns the value.
+        /// </summary>
+        /// <returns>The number of gang members that the palyer owns.</returns>
         public int allocateGangMembers()
         {
             this.noOfGangMembers += ownedTiles.Count;
             return noOfGangMembers;
-             
-
         }
-
-
     }
-
-
-   
-
 }
