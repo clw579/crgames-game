9353451b29b2e56805f897d3c40f2b722e3ff894
Thomas Lee <thomasslee97@gmail.com>
2017-12-20 16:15:55 +0000

    Tile Interaction Commenting

--- Assets/Scripts/TileInteraction.cs
+++ Assets/Scripts/TileInteraction.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/*
+	CLASS: TileInteraction
+	FUNCTION: Handles players interacting with a tile (mouse over, clicking)
+ */
+
 namespace CRGames_game
 {
 	public class TileInteraction : MonoBehaviour {
@@ -27,10 +32,13 @@ namespace CRGames_game
 			// Find the GameManager
 			manager = GameObject.FindWithTag("MainCamera").GetComponent<GameManager>();
 
+			// Get the SpriteRenderer attached to this tile
 			myRenderer = gameObject.GetComponent<SpriteRenderer>();
 
+			// Reset the colour of this tile
 			tile.resetColor(manager.getCollegeColours());
 
+			// Find the label of this tile
 			foreach (Transform child in transform)
 			{
 				if (child.gameObject.tag == "Label"){
@@ -43,10 +51,14 @@ namespace CRGames_game
 
 		// Update is called once per frame
 		void Update () {
+			// If the tile has at least one gang member, set the counter on the tile to the number of gang members
 			if (tile.getGangStrength() > 0){
+				// Show the counter label
 				textObject.SetActive(true);
+				// Set the counter label to the gang strength
 				textObject.GetComponent<TextMesh>().text = tile.getGangStrength().ToString();
 
+				// If a gang member has not been shown on the tile, show one
 				if (!memberShown){
 					CreateGangMember();
 
@@ -54,6 +66,7 @@ namespace CRGames_game
 				}
 			}
 
+			// If there are no gang members on the tile, hide the label and the gang member sprite
 			if (tile.getGangStrength() == 0){
 				textObject.SetActive(false);
 
@@ -62,49 +75,50 @@ namespace CRGames_game
 			}
 		}
 
+		/// <summary>
+		/// Creates a gang member on the tile.
+		/// </summary>
 		void CreateGangMember(){
 
             // Instantiate a copy of the gang member sprite
             GameObject member = Instantiate<GameObject>(gangMemberSprite) as GameObject;
 
-            // assgin the spriteRender and animtor components
+            // Assgin the spriteRender and animtor components
             SpriteRenderer rend = member.GetComponent<SpriteRenderer>() as SpriteRenderer;
             Animator anim = member.GetComponent<Animator>() as Animator;
 
 			// Set gang member's parent to the tile
 			member.transform.parent = transform;
 
-            // move the poisiton on the sprites
+            // Move the poisiton on the sprites
             member.transform.localPosition = new Vector3(0.0f, -0.1f, -5.0f);
             
-            // play the gangMembers animation
+            // Play the gangMembers animation
             anim.Play("gooseAnimation", -1, 0.0f);
 
-            //update the gangmembers
+            // Update the gangmembers
             myGangMember = member;
 		}
-
-        /// <summary>
-        /// When clicked, report the click to the GameManager
-        /// </summary>
-        void OnMouseDown()
-        {
-            
-        }
 	    
 		/// <summary>
-		/// Unhighlight when the mouse moves out
+		/// Unhighlight when the mouse moves out.
 		/// </summary>
 	    void OnMouseExit() {
 	        gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
 			gameObject.transform.GetChild(0).gameObject.SetActive(false);
 	    }
 
+		/// <summary>
+		/// Highlight the tile when moused over and detect click events.
+		/// </summary>
 		void OnMouseOver()
 		{
+			// Increase the tile size
 			gameObject.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+			// Activate the tile highlighter
 			gameObject.transform.GetChild(0).gameObject.SetActive(true);
 
+			// Detect left and right click over tile
 			if (!EventSystem.current.IsPointerOverGameObject())
             {
 				if(Input.GetMouseButtonDown(1)){
@@ -116,6 +130,10 @@ namespace CRGames_game
             }
 		}
 
+		/// <summary>
+		/// Sets the sprite to use to represent gang members.
+		/// </summary>
+		/// <param name="gangMemberSprite">The sprite to use for gang members.</param>
 		public void SetGangMemberSprite(GameObject gangMemberSprite){
 	
             this.gangMemberSprite = gangMemberSprite;
