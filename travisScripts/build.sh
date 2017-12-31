cbcc71f85fddc6729edcf9420101a3c9384196ca
Alex Newton <xandernewton@hotmail.com>
2017-12-31 12:06:08 +0000

    Updates the Travis build Scripts to run unit tests

--- travisScripts/build.sh
+++ travisScripts/build.sh
@@ -1,5 +1,24 @@
 project="crgames-game"
 
+
+## Run the editor unit tests
+echo "Running editor unit tests for $project"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath $(pwd) \
+	-runEditorTests \
+	-editorTestsResultFile $(pwd)/test.xml \
+	-quit
+
+rc0=$?
+echo "Unit test logs"
+cat $(pwd)/test.xml
+# exit if tests failed
+if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
